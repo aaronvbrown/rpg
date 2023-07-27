c9d8b259b48e69b97cc7056116537702272438d6
Aaron Brown <worldofaaron@hotmail.com>
2023-07-26 18:49:03 -0700

    red phase - bad fail

---
+++ src/character.js
@@ -0,0 +1,9 @@
+export default class Character {
+  // constructor(name, strength, intelligence) {
+  //   this.name = name;
+  //   this.strength = strength;
+  //   this.intelligence = intelligence;
+  //   this.level = 1;
+  //   this.inventory = [];
+  // }
+}
