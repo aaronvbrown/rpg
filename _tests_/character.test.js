74f868683a1e989c98ea04aeb8bbafec029103d6
Aaron Brown <worldofaaron@hotmail.com>
2023-07-26 19:31:54 -0700

    green phase - attack logic passes

--- _tests_/character.test.js
+++ _tests_/character.test.js
@@ -3,28 +3,36 @@ import Character from "./../src/character.js"
 
 describe("Character", () => {
   let character;
-
+  
   beforeEach(() => {
-    character = new Character("Neo", 5, 5);
+    character = new Character("Neo", 5, 5, 5);
     return character;
   });
-
+  
+  // beforeEach(() => {
+  //   return ops;
+  // });
+  
   test("should have a name", () => {
     expect(character.name).toEqual("Neo");
   });
-
+  
   test("should have a strength attribute", () => {
     expect(character.strength).toEqual(5);
   });
-
+  
   test("should have a intelligence attribute", () => {
     expect(character.intelligence).toEqual(5);
   });
-
-  test("should be able to attack a monster", () => {
-    const monster = new Character("Killer Bunny", 3, 3);
-    character.attack(monster);
-    expect(monster.health).toEqual(3);
+  
+  test("should have a health attribute", () => {
+    expect(character.health).toEqual(5);
   });
   
+  test("should be able to attack the ops", () => {
+    const ops = new Character("Killer Bunny", 5, 5, 5);
+    character.attack(ops);
+    expect(ops.health).toEqual(0);
+  });
+
 });
--- src/character.js
+++ src/character.js
@@ -1,9 +1,15 @@
 export default class Character {
-  constructor(name, strength, intelligence) {
+  constructor(name, strength, intelligence, health) {
     this.name = name;
     this.strength = strength;
     this.intelligence = intelligence;
+    this.health = health
     this.level = 1;
     this.inventory = [];
+  }
+
+  attack(ops) {
+    const damage = this.strength * this.level;
+    ops.health -= damage;
   }
 }
